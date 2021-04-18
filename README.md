4148724c9fd14a9625273cff513c29e1ab320db6
ilhamsobari <ilhamshobari25@gmail.com>
2021-04-18 17:12:07 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fragmentilham
+# fragmentilham
---
+++ app/src/main/java/id/ac/pelitabangsa/fragmentilham/SekondActivity.java
@@ -0,0 +1,28 @@
+package id.ac.pelitabangsa.fragmentilham;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SekondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Log.v("CREATING", "SECOND ACTIVITY");
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null){
+            SecondFragment details = new SecondFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
+
+}
